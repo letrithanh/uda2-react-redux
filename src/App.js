72ab4015ca07cff8fb63010d4d8f8941dd2573c0
Thanh Le <letrithanh@outlook.com>
2023-06-30 20:41:58 +0700

    prevent unauthorized user visits another pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import "./App.css";
-import NavigationBar, { HOME_PATH, LEADER_BOARD_PATH, LOGIN_PATH, NEW_PATH, NOT_FOUND_WILDCARD, QUESTION_PATH, QUESTION_PATH_PARAM } from "./components/nav";
+import NavigationBar, {
+    HOME_PATH,
+    LEADER_BOARD_PATH,
+    LOGIN_PATH,
+    NEW_PATH,
+    NOT_FOUND_WILDCARD,
+    QUESTION_PATH,
+    QUESTION_PATH_PARAM,
+} from "./components/nav";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./components/home";
 import Login from "./components/login";
@@ -8,28 +16,37 @@ import LeaderBoard from "./components/leader-board";
 import New from "./components/new";
 import { useEffect } from "react";
 import Question from "./components/question";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function App() {
-    
     const location = useLocation();
+    const userId = useSelector((state) => state.user.info?.id);
+    const navigate = useNavigate();
 
     useEffect(() => {
-        document.body.classList.add('h-full');
-        document.body.classList.add('bg-gray-50');
+        document.body.classList.add("h-full");
+        document.body.classList.add("bg-gray-50");
     }, []);
+
+    useEffect(() => {
+        const isNotRedirectLoginPath = location.pathname === "" || location.pathname === HOME_PATH || location.pathname === LOGIN_PATH;
+        if (!isNotRedirectLoginPath && !userId) {
+            navigate(LOGIN_PATH);
+        }
+    }, [userId, location]);
 
     function isNavigationBarShow() {
         const pathName = location.pathname;
-        return [HOME_PATH, LEADER_BOARD_PATH, NEW_PATH].includes(pathName) ||
-                pathName.includes(QUESTION_PATH_PARAM)
+        return (
+            [HOME_PATH, LEADER_BOARD_PATH, NEW_PATH].includes(pathName) ||
+            pathName.includes(QUESTION_PATH_PARAM)
+        );
     }
 
     return (
         <div className="h-full">
-            {
-                isNavigationBarShow() &&
-                <NavigationBar />
-            }
+            {isNavigationBarShow() && <NavigationBar />}
 
             <Routes>
                 <Route exact path={HOME_PATH} element={<Home />} />
