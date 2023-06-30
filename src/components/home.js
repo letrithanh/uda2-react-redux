1be042d267999b980371a1d580dec550d771667e
Thanh Le <letrithanh@outlook.com>
2023-06-30 19:08:36 +0700

    filter question to a correct section

--- src/components/home.js
+++ src/components/home.js
@@ -6,47 +6,61 @@ import { closeQuestion, update } from "../slices/question";
 import QuestionCard from "./card/question-card";
 
 const Home = () => {
-
-    const userId = useSelector((state) => state.user.info?.id);
-    const questionInfo = useSelector((state) => state.question)
-    const openningQuestion = useSelector((state) => state.question.openningQuestion)
+    const user = useSelector((state) => state.user.info);
+    const questionInfo = useSelector((state) => state.question);
+    const openningQuestion = useSelector(
+        (state) => state.question.openningQuestion
+    );
     const dispatch = useDispatch();
 
     useEffect(() => {
         const fetchQuestions = async () => {
             const resQuestions = await _getQuestions();
-            dispatch(update(Object.values(resQuestions)))
-        }
+            dispatch(update(Object.values(resQuestions)));
+        };
         fetchQuestions();
-    }, [dispatch])
+    }, [dispatch]);
+
+    function newQuestions() {
+        if (!questionInfo.all || !user) {
+            return [];
+        }
+
+        const answeredQuestions = Object.keys(user.answers);
+        return questionInfo.all.filter(question => !answeredQuestions.includes(question.id), []);
+    }
+
+    function doneQuestions() {
+        if (!questionInfo.all || !user) {
+            return [];
+        }
+
+        const answeredQuestions = Object.keys(user.answers);
+        return questionInfo.all.filter(question => answeredQuestions.includes(question.id), []);
+    }
+
 
     return (
         <div className="p-8 relative flex flex-col gap-4">
-            {
-                questionInfo.all?.length > 0 &&
+            {questionInfo.all?.length > 0 && (
                 <div>
-                    <PollCardSection 
-                        name="New Questions" 
-                        questions={questionInfo.all}
+                    <PollCardSection
+                        name="New Questions"
+                        questions={newQuestions()}
                     />
                 </div>
-            }
-            {
-                userId && questionInfo.all?.length > 0 &&
-                <PollCardSection 
-                    name="Done" 
-                    questions={questionInfo?.all}
-                />
-            }
-            {
-                userId && openningQuestion &&
+            )}
+            {user?.id && questionInfo.all?.length > 0 && (
+                <PollCardSection name="Done" questions={doneQuestions()} />
+            )}
+            {user?.id && openningQuestion && (
                 <div className="absolute z-50 top-0 left-0 bg-gray-50 h-full w-full p-8">
-                    <QuestionCard 
+                    <QuestionCard
                         question={openningQuestion}
                         onClose={() => dispatch(closeQuestion())}
                     />
                 </div>
-            }
+            )}
         </div>
     );
 };
